c9ab30668300adf223afabd0fc214abe6ee4c672
Mr. Kibs <32917020+kiba095@users.noreply.github.com>
2025-05-10 21:31:56 +0800

    for localserver production
    reconfig settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q3ka=t48_&i^jwnfcr$_end&!tyk28g*r)3eqr5cz8xql)@7zr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
